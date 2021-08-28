4a92de39710169fc0358daaf9993d1d595fc1ffa
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-08-28 22:12:05 +0100

    Apply formatting

--- openfisca_uk_data/datasets/__init__.py
+++ openfisca_uk_data/datasets/__init__.py
@@ -2,10 +2,10 @@ from openfisca_uk_data.datasets.frs import *
 from openfisca_uk_data.datasets.spi import *
 
 DATASETS = (
-	FRS,
-	BaseFRS,
-	SynthFRS,
-	FRS_SPI_Adjusted,
-	SPI,
-	BaseSPI,
+    FRS,
+    BaseFRS,
+    SynthFRS,
+    FRS_SPI_Adjusted,
+    SPI,
+    BaseSPI,
 )
