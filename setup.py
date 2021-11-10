ec6ea5312746ae681b16cb2bba67f5f9143882d8
Nikhil <nikhil@policyengine.org>
2021-11-10 12:31:13 +0000

    Bump version (major)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-UK-Data",
-    version="0.2.0",
+    version="0.3.0",
     description=(
         "A Python package to manage OpenFisca-UK-compatible microdata"
     ),
