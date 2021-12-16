7db8329795f37e77e4df6f3b8089c518c88b38b8
Nikhil <nikhil@policyengine.org>
2021-12-16 23:45:53 +0000

    Add country role

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-UK-Data",
-    version="0.5.2",
+    version="0.5.3",
     description=(
         "A Python package to manage OpenFisca-UK-compatible microdata"
     ),
