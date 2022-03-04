ec916577045f851afc4e2cd9c3ebc50770764f30
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-03-04 16:57:50 +0000

    Remove extraneous testing code

--- openfisca_uk_data/datasets/frs/__init__.py
+++ openfisca_uk_data/datasets/frs/__init__.py
@@ -1,7 +1,5 @@
 from openfisca_uk_data.utils import dataset
 from openfisca_uk_data.datasets.frs.raw_frs import RawFRS
-from openfisca_uk_data.datasets.frs.ukmod.ukmod_input import UKMODInput
-from openfisca_uk_data.datasets.frs.ukmod.ukmod_output import UKMODOutput
 from openfisca_uk_data.datasets.frs.frs import FRS
 from openfisca_uk_data.datasets.frs.frs_spi_adjusted import FRS_SPI_Adjusted
 from openfisca_uk_data.datasets.frs.frs_was_imputation import (
