0e3ed96b8acbe56b92991d2d7d24beb1c458bb76
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2022-03-29 12:49:29 +0100

    Fix test

--- openfisca_uk_data/tests/frs/test_synth_frs.py
+++ openfisca_uk_data/tests/frs/test_synth_frs.py
@@ -11,5 +11,5 @@ def test_synth_works_with_openfisca_uk():
     from openfisca_uk import Microsimulation
     from openfisca_uk_data import SynthFRS
 
-    sim = Microsimulation(dataset=SynthFRS, year=2019)
+    sim = Microsimulation(dataset=SynthFRS, year=2019, adjust_weights=False)
     assert not sim.calc("net_income", 2022).isna().any()
